c6e9fe74d3e6bc518f9486e89456b694b8eb6299
Aleksandr Muzhev <muzhev@arnica.pro>
2024-11-12 13:08:41 +0500

    add new test с промокодом

--- test_dir/test.ts
+++ test_dir/test.ts
@@ -7,34 +7,33 @@ test.describe.configure({ mode: 'parallel', retries: 1});
 
 let page;
 
-test.beforeAll(async ({ browser }) => {
-  page = await browser.newPage();
-  const loginPage = await new LoginPage(page);
-  await loginPage.goto();
-  await loginPage.getEmailInput.fill('test@email.ru');
-  await loginPage.getPasswordInput.fill('123456');
-  await loginPage.toLogin();
-});
+// test.beforeAll(async ({ page }) => {
+  // page = await browser.newPage();
+  // const loginPage = await new LoginPage(page);
+  // await page.goto("https://basket.genotek.ru/");
+  // await loginPage.getEmailInput.fill('test@email.ru');
+  // await loginPage.getPasswordInput.fill('123456');
+  // await loginPage.toLogin();
+// });
 
-test.afterEach(async ({}, testInfo) => {
-  // Runs after each test and save url on failure.
-  if (testInfo.status == 'failed' || testInfo.status == 'passed') {
-    const page_url = page.url();
-    let page_path = page_url.replace('https://dev-lk.genotek.ru', '');
-    page_path = page_path.replace(/\?.*/, '');
-    allure.link({ url: page_path, name: "execution_point" });
-  }
-});
+// test.afterEach(async ({}, testInfo) => {
+//   // Runs after each test and save url on failure.
+//   if (testInfo.status == 'failed' || testInfo.status == 'passed') {
+//     const page_url = page.url();
+//     let page_path = page_url.replace('https://dev-lk.genotek.ru', '');
+//     page_path = page_path.replace(/\?.*/, '');
+//     allure.link({ url: page_path, name: "execution_point" });
+//   }
+// });
 
-test.afterAll(async () => {
-  await page.close();
-});
+// test.afterAll(async () => {
+//   await page.close();
+// });
 
 const userIds = [260959];
 
 for (const userId of userIds) {
   test(`Log in as a User_${userId}`, async () => {
-
     test.info().annotations.push({ 
       type: 'As an', 
       description: 'Client'
@@ -58,3 +57,29 @@ for (const userId of userIds) {
 
   });
 }
+
+test.only('Тест на добавление товара "Происхождение" с промокодом', async ({ page }) => {
+  // Переход на сайт
+  await page.goto('https://basket.genotek.ru/order?roistat_visit=20318117');
+
+  // Выбор товара "Происхождение"
+  const titleLocator = page.locator('.app-button--primary').first();
+  await titleLocator.click();
+
+  // Добавление промокода
+  const promo = page.locator('//div[contains(text(), "У меня есть промокод")]');
+  await promo.click();
+  const typePromo = page.getByPlaceholder('Введите промокод');
+  await typePromo.fill("genotek5");
+  const enterPromo = page.locator('.basket-promo-code__control > div');
+  await enterPromo.click();
+ 
+  // Проверка, что стоимость товара зачеркнута
+  const oldPrice = await page.locator('basket-order__bill-price--crossed'); 
+  await oldPrice.isVisible();
+  // Проверка, что стоимость товара уменьшилась
+  const newPrice = await page.locator('.basket-order__bill-prices > div:nth-child(2)');
+  const actual = await newPrice.textContent();
+  const expected = "6649.05 ₽";
+  expect(actual).toBe(expected);
+});
